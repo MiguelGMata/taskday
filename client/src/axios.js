16e13634168310f9d73d66f7a710dc20311aa8e8
MiguelGMata <miguelgmata.dev@gmail.com>
2024-10-04 17:35:38 +0200

    test production

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: process.env.NODE_ENV === 'production' ? 'https://taskday-4b5229bdc00d.herokuapp.com/' : 'http://localhost:4000',
+    baseURL: process.env.NODE_ENV === 'production' ? 'https://taskday-4b5229bdc00d.herokuapp.com/api' : 'http://localhost:4000/api',
     headers: {
         'Content-Type': 'application/json'
     }
@@ -9,6 +9,6 @@ const apiClient = axios.create({
 
 
 export const getDataUsers = async () => {
-    const response = await apiClient.get('/api/users');
+    const response = await apiClient.get('/users');
     return response.data;
 }
