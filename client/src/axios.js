18aa8cb914a639dd67f137b59630cd09e5947011
MiguelGMata <miguelgmata.dev@gmail.com>
2024-10-08 16:36:35 +0200

    Asegurando que mysql está en las dependencias

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: process.env.NODE_ENV === 'production' ? 'https://taskday-4b5229bdc00d.herokuapp.com/api' : 'http://localhost:4000/api',
+    baseURL: process.env.NODE_ENV === 'production' ? 'https://taskday-kappa.vercel.app/api' : 'http://localhost:4000/api',
     headers: {
         'Content-Type': 'application/json'
     }
