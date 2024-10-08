c0689ea773e2496fe94cf927fe8190f6f302be2d
MiguelGMata <miguelgmata.dev@gmail.com>
2024-10-08 16:26:43 +0200

    Asegurando que mysql está en las dependencias

--- config/config.js
+++ config/config.js
@@ -21,6 +21,7 @@ module.exports = {
         "database": process.env.DB_NAME,
         "host": process.env.DB_HOST,
         "dialect": "mysql",
+        "dialectModule": "mysql2",
         logging: console.log,
     }
 }
