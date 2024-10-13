9287d1103d9bbeb81a10f0eda23c77847a21fa25
MiguelGMata <miguelgmata.dev@gmail.com>
2024-10-13 17:16:25 +0200

    run build and dist

--- client/src/components/services/axios.js
+++ client/src/components/services/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: process.env.NODE_ENV === 'production' ? 'https://taskday-kappa.vercel.app' : 'http://localhost:4000',
+    baseURL: process.env.NODE_ENV === 'production' ? 'https://taskday-france.vercel.app' : 'http://localhost:4000',
     headers: {
         'Content-Type': 'application/json'
     }
